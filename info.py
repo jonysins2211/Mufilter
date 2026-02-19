a6124b07d2f202f08de1637a9d91c38e70e74550
jonysins2211 <Jonysins11220@gmail.com>
2026-02-19 14:07:23 +0530

    Update info.py

--- info.py
+++ info.py
@@ -200,7 +200,7 @@ BAD_WORDS = {
 # Server & Web Configuration
 # ============================
 
-NO_PORT = bool(environ.get('NO_PORT', False))
+NO_PORT = bool(environ.get('NO_PORT', True))
 APP_NAME = None
 if 'DYNO' in environ:
     ON_HEROKU = True
@@ -209,7 +209,7 @@ else:
     ON_HEROKU = False
 BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
 FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
-URL = "https://{}/".format(FQDN) if ON_HEROKU or NO_PORT else "https://{}/".format(FQDN, PORT)
+URL = "https://disabled-tandi-josh2121-e71f4e6a.koyeb.app/".format(FQDN) if ON_HEROKU or NO_PORT else "https://disabled-tandi-josh2121-e71f4e6a.koyeb.app/".format(FQDN, PORT)
 SLEEP_THRESHOLD = int(environ.get('SLEEP_THRESHOLD', '60'))
 WORKERS = int(environ.get('WORKERS', '4'))
 SESSION_NAME = str(environ.get('SESSION_NAME', 'dreamXBotz'))
@@ -223,9 +223,9 @@ else:
     ON_HEROKU = False
 HAS_SSL = bool(getenv('HAS_SSL', True))
 if HAS_SSL:
-    URL = "https://{}/".format(FQDN)
+    URL = "https://disabled-tandi-josh2121-e71f4e6a.koyeb.app/".format(FQDN)
 else:
-    URL = "http://{}/".format(FQDN)
+    URL = "https://disabled-tandi-josh2121-e71f4e6a.koyeb.app/".format(FQDN)
 
 # ============================
 # Reactions Configuration
